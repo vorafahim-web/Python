f680109c7ce2bf2b525fcc28babdac5f7a443cc0
vorafahim-web <vorafahim5@gmail.com>
2026-08-09 11:10:06 +0530

    Add comparison program to find the largest number

---
+++ p7.py
@@ -0,0 +1,9 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if a > b:
+    print("Largest:", a)
+elif b > a:
+    print("Largest:", b)
+else:
+    print("Both are equal")
