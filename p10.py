faed74ea3dd74b6ad2c2db16b9fa51017bd8d3b9
vorafahim-web <vorafahim5@gmail.com>
2026-08-09 11:15:57 +0530

    Add grading system based on input marks

---
+++ p10.py
@@ -0,0 +1,14 @@
+marks = float(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+elif marks >= 60:
+    print("Grade D")
+elif marks >= 40:
+    print("Grade E")
+else:
+    print("Fail")
